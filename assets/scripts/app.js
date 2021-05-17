023aa19452f18a6767c2a3fe6d8e9cfc1488aaa3
unknown <j0h@example.com>
2021-05-17 18:46:06 +0300

    Using & Connecting Multiple Classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,8 +13,32 @@ class Product {
   }
 }
 
-const productList = {
-  products: [
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  //logic of rending single product in product item
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}" >
+        <div class="product-item__content">
+          <h2>${this.product.title}</h2>
+          <h3>\$${this.product.price}</h3>
+          <p>${this.product.description}</p>
+          <button>Add to Cart</button>
+        </div>
+      </div>
+    `;
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
     new Product( //new product object
       'A Pillow',
       'https://www.maxpixel.net/static/photo/2x/Soft-Pillow-Green-Decoration-Deco-Snuggle-1241878.jpg',
@@ -27,31 +51,23 @@ const productList = {
       'A carpet which you might like - or not.',
       89.99
     )
-  ],
-  //render list of products
+  ];
+
+  constructor() {}
+
   render() {
     const renderHook = document.getElementById('app'); //render in app (which is inside divs)
     const prodList = document.createElement('ul');
     prodList.className = 'product-list'; // add class to prodList
     //logic for render single product 
     for (const prod of this.products) {
-      const prodEl = document.createElement('li');
-      prodEl.className = 'product-item';
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}" >
-          <div class="product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>\$${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);   //to go throught the product list
+      const prodEl = productItem.render(); //render return new object
       prodList.append(prodEl);
-    }
+    } 
     renderHook.append(prodList);//append some content 
   }
 };
 
+const productList = new ProductList();
 productList.render();
